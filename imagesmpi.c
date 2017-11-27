9cffbadcad59a19950e7233e825cb13c78e1ef2d
Eli Ruiz <eruiz063@fiu.edu>
2017-11-27 14:46:04 -0500

    Moved some files around

--- imagesmpi.c
+++ imagesmpi.c
@@ -1,26 +1,33 @@
+/*
+	
+	This is an affirmation of originality.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <ctype.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
-int main(int argc, char const *argv[])
-{
+int main(int argc, char ** argv) {
 	int fd[2];
 	pid_t pid;
 	pipe(fd);
 	pid = fork();
 
 	switch(pid){
-		case 0:{
-
+		case 0: { /*	Child 1		*/		
+			execlp("mkdir","mkdir","results", (int*)0);	
+			break;
 		}
 		case -1:{
 			perror("fork failed");
 			exit(-1);
+		}
+		default: { /*	Parent	*/
+			
+			break;
 		}
 	}
 	return 0;
