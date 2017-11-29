eb4f90f3dd161420ed9547409e018e7d92245bdc
Kennet Postigo <kpost009@fiu.edu>
2017-11-29 14:08:58 -0500

    update results

--- imagesmpi.c
+++ imagesmpi.c
@@ -10,25 +10,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv)
+{
 	int fd[2];
 	pid_t pid;
 	pipe(fd);
 	pid = fork();
 
-	switch(pid){
-		case 0: { /*	Child 1		*/		
-			execlp("mkdir","mkdir","results", (int*)0);	
-			break;
-		}
-		case -1:{
-			perror("fork failed");
-			exit(-1);
-		}
-		default: { /*	Parent	*/
-			
-			break;
-		}
+	switch (pid)
+	{
+	case 0:
+	{ /*	Child 1		*/
+		execlp("./bmptool", "./bmptool", "-o", "./results/result.bmp", "-r", "90", "./images/1.bmp", (int *)0);
+		break;
+	}
+	case -1:
+	{
+		perror("fork failed");
+		exit(-1);
+	}
+	default:
+	{ /*	Parent	*/
+
+		break;
+	}
 	}
 	return 0;
 }
